1bd79697f92af0f324cacce7bb2dd086ba7a06a5
Nathan Ma <nathanxma@users.noreply.github.com>
2015-03-09 23:07:39 -0500

    modified account.py

--- account.py
+++ account.py
@@ -3,11 +3,15 @@ class Account(object):
     #  but not assigned to each instance
     num_accounts = 0
     def __init__(self, name, balance):
+        # scoping rules
+        # all values stored on self are part of the instance and all
+        # other assignments are just local variables
         self.name = name
         self.balance = balance
         Account.num_accounts += 1
     def __del__(self):
         Account.num_accounts -= 1
+    # deposit(), withdraw() and inquiry() are instance methods
     def deposit(self, amt):
         self.balance = self.balance + amt
     def withdraw(self, amt):
